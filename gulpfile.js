b7b205eaeb8f319474c4925251f590101361e09b
Kuldeep Singh <kuldeepsingh@chilliwacktaxi.com>
2017-12-25 14:04:14 -0800

    Readme edited

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,8 @@ var injectPartials = require('gulp-inject-Partials');
 var minify = require('gulp-minify');
 var rename = require('gulp-rename');
 var cssmin	= require('gulp-cssmin');
+var htmlmin =	require('gulp-htmlmin');
+
 
 
 var SOURCEPATHS = {
@@ -85,6 +87,12 @@ gulp.task('compresscss', function(){
 						.pipe(rename({suffix: '.min'}))
 						.pipe(gulp.dest(APPPATH.css));
 						});
+gulp.task('minifyHtml',function(){
+	return gulp.src(SOURCEPATHS.htmlSource)
+		.pipe(injectPartials())
+		.pipe(htmlmin({collapseWhitespace:true}))
+		.pipe(gulp.dest(APPPATH.root))
+});
 /** End of Production Tasks */
 gulp.task('html',function(){
 	return gulp.src(SOURCEPATHS.htmlSource)
@@ -113,3 +121,5 @@ gulp.task('watch',['serve', 'sass', 'clean-html','clean-scripts', 'scripts','mov
 });
 
 gulp.task('default', ['watch']);
+
+gulp.task('production',['minifyHtml','compresscss','compress']);
